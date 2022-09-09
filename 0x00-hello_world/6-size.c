846b710f5c357e683e28a6ef1db0b3a032ce74d1
lyndatcd <qvexvon@gmail.com>
2022-09-08 23:31:03 -0700

    Size is not grandeur

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
-
-
+/**
+ * main - Prints out the sizes of various data types on the computer
+ * this program is compiled on.
+ * Return: 0 if success.
+ */
 int main(void)
 {
 	char c;
